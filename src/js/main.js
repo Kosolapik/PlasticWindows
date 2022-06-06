8f928078fdda235c103a3ab48145afece0891c03
Kosolapik <sana.kosolapov@ya.ru>
2022-06-06 20:28:53 +0500

    added a module with a timer

--- src/js/main.js
+++ src/js/main.js
@@ -4,17 +4,20 @@ import show_modal from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
     let modalState = {};
+    let deadline = '2022-06-13';
+
 
     changeModalState(modalState);
     show_modal(modalState);
     tabs();
     forms(modalState);
-
+    timer('.container1', deadline);
     
     
 });
